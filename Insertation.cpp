966b854d379cc46878efac08a298214f9763ac17
BilkisShakila <bilkisshakilaa@gmail.com>
2025-03-25 03:04:53 +0700

    membuat fungsi void input

--- Insertation.cpp
+++ Insertation.cpp
@@ -6,7 +6,18 @@ int n; // Membuat Variable inputan n
 
 void input()
 { // procedure Input 
-    
-    
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+        cin >> n; // memanggil variable inputan n
 
+        if (n<= 20)
+        { // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+    {
+        cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n" // Menampilkan pesan jika data lebih dari 20
+    }
+}
 }
